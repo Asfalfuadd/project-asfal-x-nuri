8c330634ae7d74c86e7ad83dc61f33fbe4930877
Pall <fuadasfal32@gmail.com>
2026-05-05 22:01:34 +0800

    servis.php

--- servis.php
+++ servis.php
@@ -12,7 +12,7 @@ if(!isset($_SESSION['user'])){
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Servis</title>
+<title>Jasain.co</title>
 
     <style>
         body {
